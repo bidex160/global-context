1bf7503e3db3e9de7ffb6888880b349afafd0284
Tajudeen Wariz <wtajudeen@sysserve.com>
2024-12-19 03:57:53 +0100

    global context

---
+++ src/context.jsx
@@ -0,0 +1,14 @@
+import { createContext, useContext, useState } from "react";
+
+const GlobalContext = createContext();
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const AppContext = ({children}) => {
+    const [name, setName] = useState('test');
+    return <GlobalContext.Provider value={{name, setName}}>
+          {children}
+    </GlobalContext.Provider>
+}
+
+export default AppContext;
